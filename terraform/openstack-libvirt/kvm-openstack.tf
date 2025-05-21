84a49921851019baec893017317da6f7986df5dc
KhaledSaiidi <khaled.saiidi@outlook.com>
2025-05-21 23:21:25 +0100

    fix Ansible retry output buffering

--- terraform/openstack-libvirt/kvm-openstack.tf
+++ terraform/openstack-libvirt/kvm-openstack.tf
@@ -258,7 +258,7 @@ resource  "null_resource" "ansible_provision" {
 
   provisioner "local-exec" {
     command = <<EOF
-      ANSIBLE_FORCE_COLOR=1 ansible-playbook -v \
+      PYTHONUNBUFFERED=1 ansible-playbook \
         -i ${local_file.ansible_inventory.filename} \
         ../../ansible/openstack/site.yaml \
         || exit 1
